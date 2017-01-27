59d0a28f8e9c67ea788a2b05b6729f0886c5fb75
Gustav Solis <guso9085@colorado.edu>
2017-01-27 00:46:28 -0700

    FORMATTING

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,27 +2,6 @@ $(document).ready(function() {
   particlesJS.load('particles-js', 'particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
   });
-  /*
-  $('.container').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    arrows: false,
-    accessibility: false,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    draggable: false
-  });
-  $('#aboutbutton a').click(function(e){
-    e.preventDefault();
-    $('.container').slick('slickGoTo', 0);
-  });
-  $('#projbutton a').click(function(e){
-    e.preventDefault();
-    $('.container').slick('slickGoTo', 1);
-  });
-  */
-
   $('.projectz').slick({
     dots: true,
     draggable:false
